d65522a5b1299142a651f884580e781461035c41
hurarara <oshan1112@gmail.com>
2019-02-09 16:40:03 +0900

    Updates

---
+++ precache-manifest.cb42def2897c1a124056b71f5751cb85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdcfacafbc62971f5461",
+    "url": "/react-movie-app/static/js/runtime~main.bdcfacaf.js"
+  },
+  {
+    "revision": "ca11c0d16a4f966f21a6",
+    "url": "/react-movie-app/static/js/main.ca11c0d1.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/react-movie-app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "ca11c0d16a4f966f21a6",
+    "url": "/react-movie-app/static/css/main.9f33dfd7.chunk.css"
+  },
+  {
+    "revision": "dc08dfe397e9043a7b19f36cd625539c",
+    "url": "/react-movie-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hurarara/react-movie-app/precache-manifest.6096605b74f8ce632e66a23d1dcb96a2.js"
+  "/react-movie-app/precache-manifest.cb42def2897c1a124056b71f5751cb85.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hurarara/react-movie-app/index.html", {
+workbox.routing.registerNavigationRoute("/react-movie-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
